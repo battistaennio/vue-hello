00acec55b0ea3528699f58aa2925fce896811eae
battistaennio <ennio.battista99@gmail.com>
2024-06-24 14:27:53 +0200

    Feat: add title using Vue data

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+//metodo del framework che ci permette di creare l'app Vue
+const { createApp } = Vue;
+
+//creazione istanza dell'app
+createApp({
+    data() {
+        return {
+            text: "Hey mondo, sono il titolo scritto tramite Vue!"
+        }
+    }
+}).mount("#title")
